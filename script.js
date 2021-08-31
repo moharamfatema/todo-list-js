95e514a03a5e1fb0ca2870bacffb050e79e73d4b
Fatema Moharam <5445035-moharamfatema@users.noreply.gitlab.com>
2021-08-31 23:11:26 +0200

    handled adding to the list

--- script.js
+++ script.js
@@ -3,36 +3,34 @@ class item{
         this._isDone = false;
         this._value = value;
     }
-
     get value() {
         return this._value;
     }
-
     get isDone() {
         return this._isDone;
     }
-
-    set isDone(value) {
-        this._isDone = value;
+    set isDone(done) {
+        this._isDone = done;
     }
 }
-
-const list = [];
-
+const list = document.getElementById("list").children;
+console.log(list);
 const input = document.getElementById("textbox");
 const addButton = document.getElementById("add");
-addButton.addEventListener("click",e => function (){
-    const Item = new item(input.value);
+document.getElementsByTagName("form")[0].addEventListener("submit",e => {
+    console.log("submit")
     e.preventDefault();
-    list.push(Item);
-    updateList();
-})
 
-function updateList(){
-    const doclist = document.getElementById("list");
+    const Item = new item(input.value);
 
-    doclist.remove();
-    for(let i =0;i<list.length;i++){
-        doclist.append(list[i])
+    let li = document.createElement("li");
+    li.value = Item.value;
+    list.push(li);
+    console.log(li.value)
+    if (Item.isDone){
+        li.classList.add("done")
     }
-}
+    document.getElementById("list").appendChild(li);
+
+    handleClick(); //TODO:implement
+})
