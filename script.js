d4da49aed135f3ad3a6e84cd39e7789354d79ba2
Fatema Moharam <5445035-moharamfatema@users.noreply.gitlab.com>
2021-09-01 23:03:35 +0200

    used localstorage to restore and store

--- script.js
+++ script.js
@@ -11,8 +11,11 @@ class item{
         return this.li;
     }
 }
-const list = document.getElementById("list").children;
-console.log(list);
+const list = [];
+
+restore();
+
+
 const input = document.getElementById("textbox");
 const addButton = document.getElementById("add");
 document.getElementsByTagName("form")[0].addEventListener("submit",e => {
@@ -23,17 +26,27 @@ document.getElementsByTagName("form")[0].addEventListener("submit",e => {
         return;
     }
     const Item = new item(input.value);
-    itemAddLi(Item);
-    document.getElementById("list").appendChild(Item.li);
+    addNew(Item);
+    store(Item);
     input.value = "";
-    handleClick(Item);
-    handleContext(Item)//TODO:implement
+
 })
+function addNew(Item){
+    Item.li = itemAddLi(Item);
+    list.push(Item);
+    Item.li.id = "Li"+list.indexOf(Item).toString();
+    document.getElementById("list").append(Item.li);
+    handleClick(Item);
+    handleContext(Item);
+}
+
 function itemAddLi(Item){
     let li = document.createElement("li");
     li.innerText = Item.value;
     if (Item.isDone){
-        li.classList.add("done")
+        li.classList.add("done");
+    }else {
+        li.classList.remove("done");
     }
     console.log(li)
     Item.li = li;
@@ -45,18 +58,45 @@ function handleClick(Item){
         if (Item.isDone){
             Item.isDone = false;
             Item.li.classList.remove('done');
+            localStorage.setItem("class"+list.indexOf(Item).toString(),"undone");
         }else{
             Item.isDone = true;
             Item.li.classList.add('done');
             Item.li.order = list.length-1;
-        }
+            localStorage.setItem("class"+list.indexOf(Item).toString(),'done');
+        }console.log(localStorage);
     })
 }
 function handleContext(Item){
     Item.li.addEventListener('contextmenu', e => {
         e.preventDefault();
-        alert(Item.li.innerText + " will be removed!");
+
+        //alert(Item.li.innerText + " will be removed!");
+        let i = list.indexOf(Item);
+        localStorage.removeItem("Li"+i.toString());
+        localStorage.removeItem("class"+i.toString());
+        list.splice(i,1);
         Item.li.remove();
+
+        console.log(list);
+        console.log(localStorage);
     })
 }
-console.log(list);
+function store(Item){
+    let i = list.indexOf(Item).toString();
+    localStorage.setItem("Li"+i,Item.li.innerText);
+    if (Item.isDone){
+        localStorage.setItem("class"+list.indexOf(Item).toString(),"done");
+    }else{
+        localStorage.setItem("class"+list.indexOf(Item).toString(),"undone");
+    }
+
+}
+function restore(){
+    for (let i = 0; i < localStorage.length/2; i++){
+        const Item = new item(localStorage.getItem("Li"+i.toString()));
+        let done = localStorage.getItem("class"+i.toString());
+        Item.isDone = done === "done";
+        addNew(Item);
+    }
+}
